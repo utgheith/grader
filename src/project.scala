441ba60df5fde2dbc9deb3edcb7cfa70a25db36d
utgheith <utgheith@users.noreply.github.com>
2024-12-05 09:29:44 -0600

    publish

--- src/project.scala
+++ src/project.scala
@@ -2,6 +2,10 @@
 //> using options -Wall -Wconf:any:e -Wunused:all -feature -deprecation -unchecked -Yexplicit-nulls
 //> using jvm graalvm-java21:21
 
+//> using publish.organization com.gheith
+//> using publish.name grader
+//> using publish.version 0.1.0-SNAPSHOT
+
 //> using dep com.lihaoyi::fansi:0.5.0
 //> using dep com.lihaoyi::mainargs:0.7.6
 //> using dep com.lihaoyi::os-lib:0.11.3
