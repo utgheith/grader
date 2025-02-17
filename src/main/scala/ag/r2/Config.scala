b15d3882f6f065348c68125654e70aee91571c27
utgheith <utgheith@users.noreply.github.com>
2025-02-17 11:33:30 -0600

    r2

---
+++ src/main/scala/ag/r2/Config.scala
@@ -0,0 +1,20 @@
+package ag.r2
+
+import ag.cfg.load
+import ag.tracing.trace
+import upickle.default.ReadWriter
+
+case class Config(trace_miss: Boolean, trace_read_error: Boolean, trace_make: Boolean) derives ReadWriter {
+  def on_miss(target: Target[?])(using ctx: Context[?]): Unit = {
+    if (trace_miss) {
+      trace(s"[MISS] ${ctx.target.path} looking for ${target.path}")
+    }
+  }
+  def on_read_error(target: Target[?], e: Throwable)(using ctx: Context[?]): Unit = {
+    if (trace_read_error) {
+      trace(s"[READ ERROR] ${ctx.target.path} looking for ${target.path}: ${e.getMessage}")
+    }
+  }
+}
+
+val config = load[Config]
