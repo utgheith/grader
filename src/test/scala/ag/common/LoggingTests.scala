f35e540baf20291bd81c7b6d5850fedb4213b048
utgheith <utgheith@users.noreply.github.com>
2025-11-19 11:35:03 -0600

    LoggingTest

---
+++ src/test/scala/ag/common/LoggingTests.scala
@@ -0,0 +1,14 @@
+package ag.common
+
+import munit.FunSuite
+import scala.collection.mutable.ListBuffer
+
+class LoggingTests extends FunSuite {
+  test("logging") {
+    val logs = ListBuffer[Any]()
+    given logger: Logging = logs += _
+    log("test message")
+    log(5)
+    assert(clue(logs.toList) == List("test message", 5))
+  }
+}
